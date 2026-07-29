c8c40e5febafc9dbc9b8ee43708183a7444c9cad
Aditya Mohan <adityamohan423@gmail.com>
2026-07-29 10:45:23 +0530

    Time: 73 ms (14.41%) | Memory: 29.2 MB (29.83%) - LeetSync

--- 15-3sum/3sum.cpp
+++ 15-3sum/3sum.cpp
@@ -2,27 +2,34 @@ class Solution {
 public:
     vector<vector<int>> threeSum(vector<int>& nums) {
         int n = nums.size();
-        
-        unordered_map<int,int> mpp;
-        set<vector<int>> st;
-        vector<int> v(3);
-
-        for(int i = 0; i < n; i++){
-            mpp.clear();
-            for(int j = i+1; j < n; j++){
-                int temp = -1*(nums[i]+nums[j]);
-                if(mpp.find(temp) != mpp.end()){
-                    v = {nums[i],temp,nums[j]};
-                    sort(v.begin(),v.end());
-                    st.insert(v);
-                }
-                mpp[nums[j]]++;
-            }
-        }
-
+        int i = 0, j = 1, k = n-1;
+        sort(nums.begin(),nums.end());
         vector<vector<int>> ans;
-        for(auto it:st){
-            ans.push_back(it);
+
+        while(i < n){
+            if(i != 0){
+                while(i < n && nums[i] == nums[i-1]) ++i;
+            }
+            j = i+1;
+            k = n-1;
+
+            while(j < k){
+                if(j != i+1) while(j < k && (nums[j] == nums[j-1])) ++j;
+                if(k != n-1) while(k > j && (nums[k] == nums[k+1])) --k;
+
+                if(j >= k) break;
+                int sum = nums[i] + nums[j] + nums[k];
+                if(sum < 0){
+                    j++;
+                }else if(sum > 0){
+                    k--;
+                }else{
+                    ans.push_back({nums[i],nums[j],nums[k]});
+                    j++;
+                    k--;
+                }
+            }
+            i++;
         }
 
         return ans;
