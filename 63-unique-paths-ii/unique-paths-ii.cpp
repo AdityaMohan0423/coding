dbf65c91165e5c4a85f92473373034b6ccbf5509
Aditya Mohan <adityamohan423@gmail.com>
2026-08-07 16:26:01 +0530

    Time: 0 ms (100.00%) | Memory: 11.9 MB (32.31%) - LeetSync

--- 63-unique-paths-ii/unique-paths-ii.cpp
+++ 63-unique-paths-ii/unique-paths-ii.cpp
@@ -3,23 +3,34 @@ public:
     int uniquePathsWithObstacles(vector<vector<int>>& obstacleGrid) {
         int m = obstacleGrid.size();
         int n = obstacleGrid[0].size();
-        vector<vector<int>> dp(m,vector<int> (n,-1));
+        // vector<vector<int>> dp(m,vector<int> (n,-1));
 
 
-        function<int(int,int)> dfs = [&](int i, int j){
-            if(i >= m || j >= n) return 0;
-            if(obstacleGrid[i][j] == 1) return 0;
-            if(i == m-1 && j == n-1) return 1;
+        // function<int(int,int)> dfs = [&](int i, int j){
+        //     if(i >= m || j >= n) return 0;
+        //     if(obstacleGrid[i][j] == 1) return 0;
+        //     if(i == m-1 && j == n-1) return 1;
             
-            if(dp[i][j] != -1) return dp[i][j];
+        //     if(dp[i][j] != -1) return dp[i][j];
 
-            int path1 = 0,path2 = 0;
-            if(i < m) path1 = dfs(i+1,j);
-            if(j < n) path2 = dfs(i,j+1);
+        //     int path1 = 0,path2 = 0;
+        //     if(i < m) path1 = dfs(i+1,j);
+        //     if(j < n) path2 = dfs(i,j+1);
 
-            return dp[i][j] = path1 + path2;
-        };
+        //     return dp[i][j] = path1 + path2;
+        // };
+        // return dfs(0,0);
+        vector<vector<int>> dp(m,vector<int> (n,0));
+        if(obstacleGrid[0][0] == 1 || obstacleGrid[m-1][n-1] == 1 ) return 0;
+        dp[0][0] = 1;
 
-        return dfs(0,0);
+        for(int i = 0; i < m; i++){
+            for(int j = 0; j < n; j++){
+                if(i > 0 && obstacleGrid[i-1][j] == 0) dp[i][j] += dp[i-1][j];
+                if(j > 0 && obstacleGrid[i][j-1] == 0) dp[i][j] += dp[i][j-1];
+            }
+        }
+
+        return dp[m-1][n-1];
     }
 };
