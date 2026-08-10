e2f9a2e24e52b0fad00112870246dc99101e6795
Aditya Mohan <adityamohan423@gmail.com>
2026-08-10 16:40:41 +0530

    Time: 87 ms (8.57%) | Memory: 21.1 MB (13.24%) - LeetSync

---
+++ 1559-cherry-pickup-ii/cherry-pickup-ii.cpp
@@ -0,0 +1,35 @@
+class Solution {
+public:
+    int cherryPickup(vector<vector<int>>& grid) {
+        int n = grid.size();
+        int m =grid[0].size();
+
+        vector<vector<vector<int>>> dp(n,vector<vector<int>>(m,vector<int>(m,-1)));
+ 
+
+        function<int(int,int, int)> dfs = [&](int i,int j1, int j2){
+            if(j1 == j2) return 0;
+            if(i == n-1) return grid[i][j1] + grid[i][j2];
+
+            if(dp[i][j1][j2] != -1) return dp[i][j1][j2];
+
+            int maxi = 0;
+            for(int j = -1; j <= 1; j++){
+                int ni = i + 1;
+                int nj = j1 + j;
+                if(ni >= 0 && ni < n && nj >= 0 && nj < m){
+                    for(int k = -1; k <= 1; k++){
+                        int njj = j2 + k;
+                        if(njj >= 0 && njj < m){
+                            maxi = max(maxi,dfs(ni,nj,njj));
+                        }
+                    }
+                }
+            }
+
+            return dp[i][j1][j2] = maxi + grid[i][j1] + grid[i][j2];
+        };
+
+        return dfs(0,0,m-1);
+    }
+};
