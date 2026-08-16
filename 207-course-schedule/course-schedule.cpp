e591ad7c7501f8c2ada8be05794eb8bc5906ea4e
Aditya Mohan <adityamohan423@gmail.com>
2026-08-16 12:52:08 +0530

    Time: 0 ms (100.00%) | Memory: 20.4 MB (12.41%) - LeetSync

--- 207-course-schedule/course-schedule.cpp
+++ 207-course-schedule/course-schedule.cpp
@@ -3,26 +3,24 @@ public:
     bool canFinish(int numCourses, vector<vector<int>>& prerequisites) {
         int n = numCourses;
         vector<vector<int>> adj(n);
-        vector<int> vis(n);
+        vector<int> vis(n,0);
 
         for(int i = 0; i < prerequisites.size(); i++){
             adj[prerequisites[i][1]].push_back(prerequisites[i][0]);
         }
 
-        unordered_set<int> st;
         function<bool(int)> dfs = [&](int node){
             vis[node] = 1;
-            st.insert(node);
 
             bool isCycle = false;
             for(int i = 0; i < adj[node].size(); i++){
                 int child = adj[node][i];
-                if(st.find(child) != st.end()) return true; 
+                if(vis[child] == 1) return true; 
 
                 if(!vis[child]) isCycle = isCycle || dfs(child);
             }
 
-            st.erase(node);
+            vis[node] = 2;
             return isCycle;
         };
 
