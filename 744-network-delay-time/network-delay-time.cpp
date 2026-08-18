a1a00550ea43cfd96e09eafb7e51d2b62a3321e5
Aditya Mohan <adityamohan423@gmail.com>
2026-08-18 20:54:53 +0530

    Time: 82 ms (59.81%) | Memory: 44.3 MB (68.96%) - LeetSync

--- 744-network-delay-time/network-delay-time.cpp
+++ 744-network-delay-time/network-delay-time.cpp
@@ -2,36 +2,42 @@ class Solution {
 public:
     int networkDelayTime(vector<vector<int>>& times, int n, int k) {
         vector<vector<pair<int,int>>> adj(n+1);
-        vector<int> dis(n+1,INT_MAX);
-
         for(int i = 0; i < times.size(); i++){
-            adj[times[i][0]].push_back({times[i][2],times[i][1]});
+            int u = times[i][0];
+            int v = times[i][1];
+            int wt = times[i][2];
+
+            adj[u].push_back({wt,v});
         }
 
-        priority_queue<pair<int,int>,vector<pair<int,int>>, greater<>> pq;
+        vector<int> dis(n+1,INT_MAX);
+        priority_queue< pair<int,int> , vector<pair<int,int>>, greater<pair<int,int>> > pq;
+
         pq.push({0,k});
         dis[k] = 0;
 
         while(!pq.empty()){
-            auto [wt,node] = pq.top(); pq.pop();
+            auto [wt,node] = pq.top();
+            pq.pop();
 
             if(wt > dis[node]) continue;
-            
-            for(int i = 0; i < adj[node].size(); i++){
-                auto [cost,child] = adj[node][i];
 
-                if(cost + wt < dis[child]){
-                    dis[child] = cost + wt;
-                    pq.push({cost+wt,child});
+            for(int i = 0; i < adj[node].size(); i++){
+                int neiWt = adj[node][i].first;
+                int nei = adj[node][i].second;
+
+                if(dis[nei] > wt + neiWt){
+                    dis[nei] = wt + neiWt;
+                    pq.push({dis[nei],nei});
                 }
             }
         }
 
-        int temp = 0;
-        for(int i = 1; i <= n; i++){
-            temp = max(temp,dis[i]);
+        int maxi = 0;
+        for(int i = 1; i < dis.size(); i++){
+            maxi = max(maxi,dis[i]);
         }
 
-        return (temp == INT_MAX ? -1 : temp);
+        return ((maxi == INT_MAX) ? -1 : maxi);
     }
 };
